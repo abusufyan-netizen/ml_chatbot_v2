5d3698e83b2ec1b9688875fcaaf2bbeaedc1fa9d
Abu Sufyan <122659223+abusufyan-netizen@users.noreply.github.com>
2025-11-04 00:30:18 +0500

    Add files via upload

--- utils/helper.py
+++ utils/helper.py
@@ -1,1 +1,18 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from PIL import Image
+import os
 
+MODEL_PATH = "model/digit_model.h5"
+
+def load_digit_model():
+    if not os.path.exists(MODEL_PATH):
+        raise FileNotFoundError("⚠️ Model not found. Train it first using train_model.py")
+    return load_model(MODEL_PATH)
+
+def preprocess_image(img):
+    img = img.convert('L').resize((28, 28))
+    img = 255 - np.array(img)
+    img = img / 255.0
+    img = img.reshape(1, 28, 28)
+    return img
